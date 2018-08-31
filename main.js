b21048737fdcd694bf586c1a9b8532f669d70e8c
Prime-Calculator <32958487+Prime-Calculator@users.noreply.github.com>
2018-08-31 12:20:13 +0200

    v1.0
    all done

---
+++ main.js
@@ -0,0 +1,40 @@
+
+window.onload = initHtmlVars;
+
+/*  SCRIPT SETTINGS  */
+hv_looping = false;
+
+/*  DEFAULT SETTINGS  */
+hv_rate = 100;
+
+function initHtmlVars() {
+    hv_loop();
+}
+
+function hv_loop() {
+    if (!hv_looping) {
+        hv_looping = true;
+        // Code...
+        var globVars = Object.entries(window);
+        var htmlTags = document.getElementsByTagName("hv");
+
+        for (var i = 0; i < globVars.length; i++) {
+            var key = globVars[i][0];
+            var val = globVars[i][1];
+
+            if (typeof val == "number" || typeof val == "string" || typeof val == "boolean") {
+                for (var j = 0; j < htmlTags.length; j++) {
+                    if (htmlTags[j].getAttribute("key") == key && htmlTags[j].innerHTML != val) {
+                        htmlTags[j].innerHTML = val;
+                    }
+                }
+            }
+        }
+
+        // Repeat...
+        setTimeout(function () {
+            hv_looping = false;
+            hv_loop();
+        }, hv_rate);
+    }
+}
